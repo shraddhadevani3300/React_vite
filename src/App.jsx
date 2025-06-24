fefd45aa02badd86b0d2c1ccbba381cbba972876
shraddhadevani3300 <shraddhadevani3300@gmail.com>
2025-06-24 08:54:27 +0530

    Initial commit or your message

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,26 @@ const App = () => {
     <>
       <div className="home-page">
         <div className="main_header">
-          <div className="header_top"></div>
+          <div className="header_top">
+            <div className="container">
+              <div className="top_inner">
+                <div className="col-right">
+                  <div className="currency">
+                    <span>USD $</span>
+                  </div>
+                  <div className="language">
+                    <span>English</span>
+                  </div>
+                </div>
+                <div className="col-left">
+                  <div className="user_info">
+                    <div className="uaccount"></div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
           <div className="header_bottom">
             <div className="container">
               <ul className="header_inner">
@@ -29,6 +48,18 @@ const App = () => {
           </div>
         </div>
         <div className="main_body">
+          <div className="banner_block">
+            <div className="banner_col">
+              <div className="image">
+                {/* <img src="./public/banner.jpg" alt="banner img" /> */}
+              </div>
+              <div className="banner_text">
+
+              </div>
+            </div>
+          </div>
+
+          {/* -----------------Static Section--------------- */}
           <div className="static_section padding">
             <div className="container">
               <div className="service_inner">
@@ -38,18 +69,18 @@ const App = () => {
                       <img src="./public/adv1.png" alt="img" />
                     </a>
                   </div>
-                  <div className="text">  
+                  <div className="text">
                     <h3>Fresh Fruits</h3>
                     <p>Up to 30% off</p>
                   </div>
                 </div>
-                 <div className="item">
+                <div className="item">
                   <div className="image">
                     <a href="#">
                       <img src="./public/adv3.png" alt="img" />
                     </a>
                   </div>
-                  <div className="text">  
+                  <div className="text">
                     <h3>Dry Fruits</h3>
                     <p>Up to 30% off</p>
                   </div>
@@ -60,7 +91,7 @@ const App = () => {
                       <img src="./adv2.png" alt="img" />
                     </a>
                   </div>
-                  <div className="text">  
+                  <div className="text">
                     <h3>Pure Honey</h3>
                     <p>Up to 30% off</p>
                   </div>
@@ -68,6 +99,8 @@ const App = () => {
               </div>
             </div>
           </div>
+          {/* -----------------Static Section--------------- */}
+
         </div>
         <div className="main-footer"></div>
       </div>
