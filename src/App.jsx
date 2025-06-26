2b34e77784dc194a222f730e019d17ba45725087
shraddhadevani3300 <shraddhadevani3300@gmail.com>
2025-06-26 08:54:36 +0530

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 const App = () => {
-
-  // https://demo4techies.com/prestashop/grocery/en/
   return (
     <>
       <div className="home-page">
@@ -16,53 +14,44 @@ const App = () => {
                     <span>English</span>
                   </div>
                 </div>
-                <div className="col-left"> 
+                <div className="col-left">
                   <div className="user_info">
                     <div className="uaccount"></div>
+                  </div>
+                </div>
                 <div className="nav-right">
                   <div className="uaccount">
                     <ul className="user_info">
-                        <li><a href="#">My Account</a></li>
-                        <li><a href="#">Register</a></li>
-                        <li><a href="#">Sign In</a></li>
+                      <li><a href="#">My Account</a></li>
+                      <li><a href="#">Register</a></li>
+                      <li><a href="#">Sign In</a></li>
                     </ul>
                   </div>
                 </div>
-
               </div>
             </div>
           </div>
+
           <div className="header_bottom">
             <div className="container">
               <ul className="header_inner">
-                <li>
-                  <a href="#">Home</a>
-                </li>
-                <li>
-                  <a href="#">Shop</a>
-                </li>
-                <li>
-                  <a href="#">Electronics</a>
-                </li>
-                <li>
-                  <a href="#">Sport&Books</a>
-                </li>
-                <li>
-                  <a href="#">Contact US</a>
-                </li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">Electronics</a></li>
+                <li><a href="#">Sport&Books</a></li>
+                <li><a href="#">Contact US</a></li>
               </ul>
             </div>
           </div>
         </div>
+
         <div className="main_body">
           <div className="banner_block">
             <div className="banner_col">
               <div className="image">
                 {/* <img src="./public/banner.jpg" alt="banner img" /> */}
               </div>
-              <div className="banner_text">
-
-              </div>
+              <div className="banner_text"></div>
             </div>
           </div>
 
@@ -81,6 +70,7 @@ const App = () => {
                     <p>Up to 30% off</p>
                   </div>
                 </div>
+
                 <div className="item">
                   <div className="image">
                     <a href="#">
@@ -92,6 +82,7 @@ const App = () => {
                     <p>Up to 30% off</p>
                   </div>
                 </div>
+
                 <div className="item">
                   <div className="image">
                     <a href="#">
@@ -103,15 +94,17 @@ const App = () => {
                     <p>Up to 30% off</p>
                   </div>
                 </div>
+
               </div>
             </div>
           </div>
           {/* -----------------Static Section--------------- */}
-
         </div>
+
         <div className="main-footer"></div>
       </div>
     </>
   );
 };
+
 export default App;
